8e6b9c8adf7e54ca067c5a152eebc9fad12e09bd
natemollica-dev <nathan.mollica@hashicorp.com>
2025-01-29 08:34:03 -0800

    Fix filename casing issues

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 export function Card({ children, className = '', ...props }) {
     return (
-        <div
-            className={`rounded-lg shadow-md border border-gray-300 p-4 bg-white hover:shadow-lg transition-shadow ${className}`}
-            {...props}
-        >
-            {children}
-        </div>
+      <div
+        className={`rounded-lg shadow-md border border-gray-300 p-4 bg-white hover:shadow-lg transition-shadow ${className}`}
+        {...props}
+      >
+        {children}
+      </div>
     );
-}
+  }
 
-export function CardContent({ children, className = '', ...props }) {
+  export function CardContent({ children, className = '', ...props }) {
     return (
-        <div className={`mt-2 text-gray-700 ${className}`} {...props}>
-            {children}
-        </div>
+      <div className={`mt-2 text-gray-700 ${className}`} {...props}>
+        {children}
+      </div>
     );
-}
+  }
--- frontend/src/pages/Dash.jsx
+++ frontend/src/pages/Dash.jsx
@@ -4,54 +4,54 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 
 const Dash = () => {
-    return (
-        <div className="container">
-            <div className="row">
-                <h3 className="text-center">Dashboard</h3>
-            </div>
-            <h5>Take these steps in order: </h5>
-            <Accordion defaultActiveKey="0">
-                <p className="textbox">
-                    <Link to="/title">1. Title Page</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/abstract">2. Abstract</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/table-of-contents">3. Table of Contents</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/list-of-figures">4. List of Figures</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/list-of-tables">5. List of Tables</Link>
-                </p>
-                <Accordion.Item eventKey="5">
-                    <Accordion.Header>
-                        <Link to="/body">6. Body (4-6 sections)</Link>{" "}
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <ul>
-                            <li>Literature Review</li>
-                            <li>Methodology</li>
-                            <li>Results</li>
-                            <li>Discussion</li>
-                            <li>Conclusion</li>
-                        </ul>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <p className="textbox">
-                    <Link to="/appendices">7. Appendices</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/references">8. References</Link>
-                </p>
-                <p className="textbox">
-                    <Link to="/other-info">9. Other Info</Link>
-                </p>
-            </Accordion>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="row">
+        <h3 className="text-center">Dashboard</h3>
+      </div>
+      <h5>Take these steps in order: </h5>
+      <Accordion defaultActiveKey="0">
+        <p className="textbox">
+          <Link to="/title">1. Title Page</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/abstract">2. Abstract</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/table-of-contents">3. Table of Contents</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/list-of-figures">4. List of Figures</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/list-of-tables">5. List of Tables</Link>
+        </p>
+        <Accordion.Item eventKey="5">
+          <Accordion.Header>
+            <Link to="/body">6. Body (4-6 sections)</Link>{" "}
+          </Accordion.Header>
+          <Accordion.Body>
+            <ul>
+              <li>Literature Review</li>
+              <li>Methodology</li>
+              <li>Results</li>
+              <li>Discussion</li>
+              <li>Conclusion</li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <p className="textbox">
+          <Link to="/appendices">7. Appendices</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/references">8. References</Link>
+        </p>
+        <p className="textbox">
+          <Link to="/other-info">9. Other Info</Link>
+        </p>
+      </Accordion>
+    </div>
+  );
 };
 
 export default Dash;
